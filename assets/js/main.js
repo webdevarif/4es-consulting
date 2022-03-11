2d6ddb807599febe9a8082bcf880b1a91df34720
Arif Hossin <webdeveloperarif@gmail.com>
2022-03-12 05:40:11 +0600

    Update Code

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,9 +49,34 @@
     if (avs_slider.length) {
 		avs_slider.each(function () {
 			var SwiperCarousel = $(this),
+            // Slides SpaceBetween
+			slidesSpaceBetween = SwiperCarousel.data('spaceBetween'),
+			slidesSpaceBetweenXl = SwiperCarousel.data('xl-spaceBetween'),
+			slidesSpaceBetweenLg = SwiperCarousel.data('lg-spaceBetween'),
+			slidesSpaceBetweenMd = SwiperCarousel.data('md-spaceBetween'),
+			slidesSpaceBetweenSm = SwiperCarousel.data('sm-spaceBetween'),
+
+            // Slides Autoplay
 			slidesAutoplay = SwiperCarousel.data('autoplay'),
+			slidesAutoplayXl = SwiperCarousel.data('xl-autoplay'),
+			slidesAutoplayLg = SwiperCarousel.data('lg-autoplay'),
+			slidesAutoplayMd = SwiperCarousel.data('md-autoplay'),
+			slidesAutoplaySm = SwiperCarousel.data('sm-autoplay'),
+
+            // Slides Loop
 			slidesLoop = SwiperCarousel.data('loop'),
+			slidesLoopXl = SwiperCarousel.data('xl-loop'),
+			slidesLoopLg = SwiperCarousel.data('lg-loop'),
+			slidesLoopMd = SwiperCarousel.data('md-loop'),
+			slidesLoopSm = SwiperCarousel.data('sm-loop'),
+
+            // Slides per View
 			slidesPerView = SwiperCarousel.data('items'),
+			slidesPerViewXl = SwiperCarousel.data('xl-items'),
+			slidesPerViewLg = SwiperCarousel.data('lg-items'),
+			slidesPerViewMd = SwiperCarousel.data('md-items'),
+			slidesPerViewSm = SwiperCarousel.data('sm-items'),
+
 			slidesSpeed = SwiperCarousel.data('speed'),
 			slidesLazy = SwiperCarousel.data('lazy'),
 			slidesNextEl = SwiperCarousel.data('next'),
@@ -62,6 +87,7 @@
 				loop: (slidesLoop ? slidesLoop : true),
 				speed: (slidesSpeed ? slidesSpeed : 600),
 				lazy: (slidesLazy ? slidesLazy : true),
+                spaceBetween: (slidesSpaceBetween ? slidesSpaceBetween : 30),
 				navigation: {
 					nextEl: slidesNextEl,
 					prevEl: slidesPrevEl,
@@ -70,6 +96,36 @@
 					crossFade: true,
 				},
 				autoplay: (slidesAutoplay ? slidesAutoplay : false),
+                breakpoints: {
+                    // when window width is >= 480px
+                    480: {
+                        slidesPerView: (slidesPerViewSm ? slidesPerViewSm : 1),
+                        loop: (slidesLoopSm ? slidesLoopSm : true),
+                        autoplay: (slidesAutoplaySm ? slidesAutoplaySm : false),
+                        spaceBetween: (slidesSpaceBetweenSm ? slidesSpaceBetweenSm : 30),
+                    },
+                    // when window width is >= 640px
+                    640: {
+                        slidesPerView: (slidesPerViewMd ? slidesPerViewMd : 1),
+                        loop: (slidesLoopMd ? slidesLoopMd : true),
+                        autoplay: (slidesAutoplayMd ? slidesAutoplayMd : false),
+                        spaceBetween: (slidesSpaceBetweenMd ? slidesSpaceBetweenMd : 30),
+                    },
+                    // when window width is >= 991px
+                    991: {
+                        slidesPerView: (slidesPerViewLg ? slidesPerViewLg : 1),
+                        loop: (slidesLoopLg ? slidesLoopLg : true),
+                        autoplay: (slidesAutoplayLg ? slidesAutoplayLg : false),
+                        spaceBetween: (slidesSpaceBetweenLg ? slidesSpaceBetweenLg : 30),
+                    },
+                    // when window width is >= 1199px
+                    1199: {
+                        slidesPerView: (slidesPerViewXl ? slidesPerViewXl : 1),
+                        loop: (slidesLoopXl ? slidesLoopXl : true),
+                        autoplay: (slidesAutoplayXl ? slidesAutoplayXl : false),
+                        spaceBetween: (slidesSpaceBetweenXl ? slidesSpaceBetweenXl : 30),
+                    },
+                },
 			});		
 		});		
 	}
@@ -233,6 +289,32 @@
 			disable: 'mobile',
 		});
 	}
+
+
+    // COlor Check
+    
+    var color_check = document.querySelectorAll(".color-check label");
+    color_check.forEach(element => {
+        element.style.backgroundColor = element.getAttribute("data-bg");
+        element.style.color = element.getAttribute("data-color");
+    });
+
+    if ($('.product-filters').length) {
+        var submenus = document.querySelectorAll(".product-filters .hassubmenu > a");
+
+        for (var i = 0; i < submenus.length; i++) {
+            submenus[i].addEventListener("click", function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+                var last = this.parentElement.classList;
+                if (last.contains('open')){
+                    last.remove("open");
+                } else{
+                    last.add("open");
+                }
+            });
+        }
+    }
 
 })(jQuery);
 
